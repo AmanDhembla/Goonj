cd413297edfd7219a3cc166e46476ff8f516045e
amandhembla@gmail.com <amandhembla@gmail.com>
2018-04-04 01:08:31 +0530

    added in the basic client setup using create-react-app

--- index.js
+++ index.js
@@ -15,9 +15,6 @@ require("./services/passport");
 //routers
 const authRouter=require("./routes/authRouter");
 
-
-
-
 app.get("/",(req,res)=>{
     res.json({"msg":"the server is working fine"});
 });
