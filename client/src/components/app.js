467dd6a72fde99e88fde33154b733e9242d57236
amandhembla@gmail.com <amandhembla@gmail.com>
2018-04-05 19:19:50 +0530

    footer aaded to the landing page

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -5,7 +5,7 @@ import Landing from './Landing';
 import Dashboard from './Dashboard';
 import {connect} from 'react-redux';
 import {fetchUser} from '../actions/index';
-
+import Footer from './Footer';
 class App extends Component {
     componentDidMount(){
         this.props.fetchUser();
@@ -16,7 +16,7 @@ class App extends Component {
                 <BrowserRouter>
                     <div>
                         <Header />
-                        <Route path="/" component={Landing} exact={true}/>
+                        <Route path="/" component={()=>(<div><Landing /><Footer /></div>)} exact={true}/>
                         <Route path="/profile" component={Dashboard} exact={true}/>
                     </div>
                 </BrowserRouter>
