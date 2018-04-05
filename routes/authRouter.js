a42345547023d302fd2450714756a8f87b27bd1a
amandhembla@gmail.com <amandhembla@gmail.com>
2018-04-05 16:00:09 +0530

    changed the order of routes in authRouter

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,12 +2,14 @@ const express=require("express");
 const passport=require("passport");
 const router=express.Router();
 
+router.get("/callback",passport.authenticate('google'),(req,res)=>{
+    res.redirect("/profile");
+});
+
 router.get("/",passport.authenticate('google',{
     scope:['profile','email']
 }));
 
-router.get("/callback",passport.authenticate('google'),(req,res)=>{
-    res.redirect("/profile");
-});
+
 
 module.exports=router;
